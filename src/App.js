2ff19a5f18cf08d9db638452e0989dc2a71b2a65
Dennis Lee <dennisdjlee@yahoo.com>
2022-06-01 15:54:04 -0300

    Version before Dual Boot PC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+  const textRef = useRef();
+
+  SpeechSynthesisUtterance.lang = "en-US"
+  var msg = new SpeechSynthesisUtterance(text);
+  msg.lang = 'pt-BR'
+
+  const clearValue = () => {
+    textRef.current.value = '';
+    setText('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="header">
+          <h3>A moça do Google Maps lê pra vc!</h3>
+        </div>
+
+        <div className="textDiv">
+          <form>
+            <div className="buttonDiv">
+              <button
+                className="readButton"
+                type="button"
+                onClick={() => window.speechSynthesis.speak(msg)}
+              >Ouvir</button>
+              <button className="clearText" type="button" onClick={clearValue}>Apagar</button>
+            </div>
+            <textarea
+              className="textArea"
+              name="textArea"
+              id="textArea"
+              cols="100"
+              rows="50"
+              ref={textRef}
+              onChange={e => setText(e.target.value)}
+              onSubmit={(e) => e.value = ''}
+              placeholder="Cola ou escreva o seu texto aqui..."
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
